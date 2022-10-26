261e95320619cc205bf3067262f7300d5c335425
Ayesssss <116766974+Ayesssss@users.noreply.github.com>
2022-10-27 05:02:32 +0700

    Add files via upload

---
+++ Check Bot.lua
@@ -0,0 +1,25 @@
+Rdp = {
+    {nordp = 1, botname = "Bot1", Status = "Nonaktif"},
+    {nordp = 1, botname = "Bot2", Status = "Nonaktif"},
+    {nordp = 2, botname = "Bot3", Status = "Nonaktif"},
+    {nordp = 2, botname = "Bot4", Status = "Nonaktif"},
+    {nordp = 3, botname = "Bot5", Status = "Nonaktif"},
+    {nordp = 3, botname = "Bot6", Status = "Nonaktif"}
+}
+
+ShowIP = true
+ip_add = {
+    "67.211.223.69",
+    "67.211.223.67",
+    "67.211.223.66"
+}
+
+hookURL = "https://discord.com/api/webhooks/1033539646099050506/yIBKcBdWKo0WI9gSYF-2bEBJvEQ9yaBuiS4WlfusxEEK5P6r2Fyc7KP8TkrDyIF6U7gu"
+
+Teks_Deskripsi = "List Nick Para Babu"
+Teks_Footer = "Golek Duit Kanggo Masa Depan"
+
+Emote_Offline = "<:offline:982957367392088124>"
+Emote_Online = "<:online:982957403756716052>"
+
+menit = 5 
